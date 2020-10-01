50d1ee34a5832ff6cfea0b0b98b3dd4c648f38c9
RomanFilipenia <70843398+RomanFilipenia@users.noreply.github.com>
2020-10-01 20:22:21 +0300

    docs : add js code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
+exports.min = (array)=> {
+  if(array.length !== 0){
+  array.sort(function(a,b){
+    return a-b;
+  })
+  return array[0];
+}else {
   return 0;
 }
-
-exports.max = function max (array) {
-  return 0;
 }
 
-exports.avg = function avg (array) {
+exports.max = (array)=> {
+  if(array.length !== 0){
+ array.sort(function(a,b){
+    return a-b;
+  })
+  return array[array.length-1];
+}else{
   return 0;
+}
+}
+
+exports.avg =  (array)=> {
+  if(array.length !== 0){
+  array.sort(function(a,b){
+    return a-b;
+  })
+    let summa=0;
+  for(let i=0; i<=array.length-1;i++){
+    summa += array[i];
+    }
+    let mid = summa/array.length;
+
+  return mid;
+  }else{
+    return 0;
+  }
 }
